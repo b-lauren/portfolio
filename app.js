44836acefb8c9150f836a0d7f4a50e6892ace0ae
Rebecca Ross <b.laurenross@gmail.com>
2021-02-03 22:00:44 +0000

    changes height on nav animation

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const navSlide = () => {
       if (link.style.animation) {
         link.style.animation = '';
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 4 + 0.5}s`;
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
       }
     });
     // burger animation
@@ -21,8 +21,8 @@ const navSlide = () => {
 
 navSlide();
 
-navLinks.forEach('click', )
+// navLinks.forEach('click', )
 
-navLinks.addEventListener("click", function() {
-  document.getElementById("demo").innerHTML = "Hello World";
-});
+// navLinks.addEventListener("click", function() {
+//   document.getElementById("demo").innerHTML = "Hello World";
+// });
